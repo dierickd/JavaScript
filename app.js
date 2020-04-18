c6dfe15177a6b739c124a281fc99753646aba6ec
dierickd <37022827+dierickd@users.noreply.github.com>
2020-04-18 18:22:44 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Forrest Gump, réalisé par Robert Zemeckis, est sorti en 1994
+
+const nameMovie = 'Forrest Gump'
+const nameDirector = 'Robert Zemeckis'
+const year = 1994
+
+const message = nameMovie + ', réalisé par ' + nameDirector + ', est sorti en ' + year
+
+alert(message)
