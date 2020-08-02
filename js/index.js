7bc0bce1bf04cdd25e487590f3a1da61c2028930
DIERICK Dewi <dierickd@outlook.fr>
2020-08-02 21:32:48 +0200

    finish quest js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import moment from "moment"
+import "../scss/style.scss"
+const $ = require('jquery')
+
+console.log(moment().startOf('day').fromNow());
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    entry: {
+        app: './js/index.js',
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    }
+};
